2544259c83de3ab9ee89fe1dfedb46be9cfb12dd
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-06-07 20:34:42 -0700

    Solved

---
+++ Day 4.playground/Contents.swift
@@ -0,0 +1,6 @@
+// This time the challenge is to create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.
+
+let array = ["John", "Alex", "Alex", "Peter"]
+let set = Set(array)
+print(array.count)
+print(set)
