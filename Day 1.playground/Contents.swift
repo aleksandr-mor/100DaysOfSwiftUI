609fe6dd81e89e4d8cdbb5878cc1223bdc55ba21
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-11-13 00:32:13 -0800

    refresh day 1

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,28 @@
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+print(name)
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+
+
+
+
+
--- Day 4.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-// This time the challenge is to create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.
-
-let array = ["John", "Alex", "Alex", "Peter"]
-let set = Set(array)
-print(array.count)
-print(set)
