b075dee158f8426460c10687cd79985497e75156
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-11-19 22:32:30 -0800

    day 1 finished

--- Day 1.playground/Contents.swift
+++ Day 1.playground/Contents.swift
@@ -25,5 +25,89 @@ var favoriteShow = "Orange is the New Black"
 favoriteShow = "The Good Place"
 favoriteShow = "Doctor Who"
 
+let actor = "Denzel Washington"
 
+let filename = "paris.jpg"
+let result = "⭐️ You Win! ⭐️"
 
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print (movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
+
+var anotherQuote = "Cgange the world by being yourself"
+
+var burns = """
+The best laid schemes
+O' mice and men
+Gang aft agley
+"""
+
+let score = 10
+let reallyBig = 100000000
+let reallyBig2 = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))
+
+let anotherNumber = 0.1 + 0.2
+print(anotherNumber)
+
+let a = 1
+let b = 2.0
+// let c = a + b
+// let c = a + Int(b)
+
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var anotherName = "Nicolas Cage"
+anotherName = "John Travolta"
+//anotherName = 57
+
+var rating = 5.0
+rating *= 2
+
+var myInt = 1
+var myDouble = 1.0
+
+var anotherMeaningOfLife = 42
+//anotherMeaningOfLife = "Forty two"
