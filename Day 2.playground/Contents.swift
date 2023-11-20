f1df7776d1da1236bc67e07213a9fe210360d086
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-11-19 23:24:32 -0800

    Day2

---
+++ Day 2.playground/Contents.swift
@@ -0,0 +1,50 @@
+let goodDogs = true
+let gameOver = false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameOver1 = false
+print(gameOver)
+
+gameOver1.toggle()
+print(gameOver1)
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sign saying \"Beliebe\" and walked away."
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name us \(name) an I'm \(age) years old."
+print(message)
+
+let number = 11
+//let missionMessage = "Apollo " + number + " landed on the moon."
+
+//let missionMessage = "Apollo " + String(number) + " landed on the moon."
+
+let missionMessage = "Aloppo \(number) landed on the moon."
+
+print("5 x 5 is \(5 * 5)")
+
+var city = "Cardiff"
+var message1 = "Welcome to \(city)!"
+
+let tempC = 40
+let tempF = tempC*9/5 + 32
+print("\(tempF)°F")
