024faa6c9f0bd152f4e6d79d43cc346ee7d80622
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-12-11 00:27:55 -0800

    Day 4

---
+++ Day 3.playground/Contents.swift
@@ -0,0 +1,20 @@
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4,8, 15, 16, 23, 42]
+let temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+beatles.append("Adrian")
+
+beatles.append("Allen")
+beatles.append("Adrian")
+beatles.append("Novall")
+beatles.append("Vivian")
+
+let firstBeatle = beatles[0]
+let firstNumber = numbers[0]
+
+//let notAllowed = firstBeatle + firstNumber
+
---
+++ Day 4.playground/Contents.swift
@@ -0,0 +1,48 @@
+let surname: String = "Lasso"
+//var score: Int = 0
+
+var score: Double = 0
+
+let playerName: String = "Roy"
+
+var luckyNumber: Int = 13
+
+let pi: Double = 3.1415
+
+var isAuthenticated: Bool = true
+
+var albulms: [String] = ["Red", "Fearless"]
+
+var user: [String: String] = ["id": "@almor"]
+
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
+
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+
+var teams: [String] = [String]()
+
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
+
+let username: String
+// lots of complex logic
+username = "@almor"
+// lots of complex logic
+print(username)
+
+var percentage: Double = 99
+
+var name: String
+
+let names = ["Eleanor", "Chidi", "Tahani", "Jianyu", "Michael", "Janet"]
+
+// Checkpoint
+let albums = ["First", "First", "Second", "Third"]
+let setOfAlbums = Set(albums)
+print(setOfAlbums.count)
