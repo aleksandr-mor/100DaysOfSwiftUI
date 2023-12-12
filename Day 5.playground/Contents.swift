0c67b1121daab4b055b6bf4a6b47ecef248ce0f3
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-12-12 00:24:52 -0800

    Day 5 in process

---
+++ Day 5.playground/Contents.swift
@@ -0,0 +1,16 @@
+//if someCondition {
+//    print("Do something")
+//}
+
+if someCondition {
+    print("Do something")
+    print("Do something else")
+    print("Do a third thing")
+}
+
+let score = 85
+
+if score > 80 {
+    print("Great job!")
+}
+
