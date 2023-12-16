9daf63499727f53deacd1140d361040fa5d37b69
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-12-15 20:43:21 -0800

    Day 5

--- Day 5.playground/Contents.swift
+++ Day 5.playground/Contents.swift
@@ -14,3 +14,18 @@ if score > 80 {
     print("Great job!")
 }
 
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print("Where we're going we don't need roads.")
+}
+
+if percentage < 85 {
+    print("Sorry, you failed the test.")
+}
+
+if age >= 18 {
+    print("You're eligible to vote")
+}
