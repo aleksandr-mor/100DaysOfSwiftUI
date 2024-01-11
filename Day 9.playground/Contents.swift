4365eb36d2d5ae2a8403698d47a38d6e0d7a08c0
Aleksandr Morozov <dr.morozov@hotmail.com>
2024-01-10 22:29:47 -0800

    Day 10 Finished

--- Day 9.playground/Contents.swift
+++ Day 9.playground/Contents.swift
@@ -85,3 +85,105 @@ let payment3 = { () -> Bool in
     return true
 }
 
+let captainFirstTeam2 = team.sorted { name1, name2 in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+}
+
+let captainFirstTeam3 = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+    
+    return $0 < $1
+}
+
+let reverseTeam = team.sorted {
+    return $0 < $1
+}
+
+let reverseTeam1 = team.sorted { $0 < $1 }
+
+let t0nly = team.filter { $0.hasPrefix("T") }
+print(t0nly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
+
+func animate(duration: Double, animations: () -> Void) {
+    print("Starting a \(duration) secont animation")
+    animations()
+}
+
+animate(duration: 3, animations: {
+    print("Fade out the image")
+})
+
+animate(duration: 3) {
+    print("Fade out the image")
+}
+
+func greetUser3() {
+    print("Hi there!")
+}
+
+greetUser()
+
+var greetCopy3: () -> Void = greetUser3
+greetCopy3()
+
+func makeArray(size: Int, using generator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    return numbers
+}
+
+let rolls = makeArray(size: 50) {
+    Int.random(in: 1...20)
+}
+
+print(rolls)
+
+func generateNumber() -> Int {
+    Int.random(in: 1...20)
+}
+
+let newRolls = makeArray(size: 50, using: generateNumber)
+
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+doImportantWork {
+    print("This is the first work")
+} second: {
+    print("This is the second work")
+} third: {
+    print("This is the third work")
+}
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let findNumber = { (_: [Int]) in
+    luckyNumbers.filter { !$0.isMultiple(of: 2) }
+        .sorted()
+        .map { print("\($0) is a lucky number.")}
+}
+findNumber(luckyNumbers)
