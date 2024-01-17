158be86015c8a4019d89090f963166ce5224a7e9
Aleksandr Morozov <dr.morozov@hotmail.com>
2024-01-16 21:09:29 -0800

    Day 10 Finish

--- Day 10.playground/Contents.swift
+++ Day 10.playground/Contents.swift
@@ -90,3 +90,128 @@ var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
 archer.vacationTaken += 4
 archer.vacationRemaining = 5
 print(archer.vacationAllocated)
+
+struct Game {
+    var score = 0
+}
+
+var game = Game()
+game.score += 10
+print("Score is now \(game.score)")
+game.score -= 3
+print("Score is now \(game.score)")
+game.score += 1
+
+struct Game1 {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game1 = Game1()
+game1.score += 10
+game1.score -= 3
+game1.score += 1
+
+struct App {
+    var contacts = [String]() {
+    willSet {
+        print("Current value is: \(contacts)")
+        print("New value will be: \(newValue)")
+    }
+    
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+
+struct Player {
+    let name: String
+    let number: Int
+}
+
+let player = Player(name: "Megan R", number: 15)
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String, number: Int) {
+        self.name = name
+        self.number = number
+    }
+}
+
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
+
+
+struct User {
+    var name: String
+    var yearsActive = 0
+}
+
+struct Employee {
+    var name: String
+    var yearsActive = 0
+}
+
+let roslin = Employee(name: "Laura Roslin")
+let adama = Employee(name: "William Adama", yearsActive: 45)
+
+struct Employee {
+    var name: String
+    var yearsActive = 0
+}
+    extension Employee {
+    init() {
+        self.name = "Anonymous"
+        print("Creating an anonymous employee...")
+    }
+}
+let roslin = Employee(name: "Laura Roslin")
+
+let anon = Employee()
+
+struct Student {
+    var name: String
+    var bestFriend: String
+    
+    init(name: String, bestFriend: String) {
+        print("Enrolling \(name) in class...")
+        self.name = name
+        self.bestFriend = bestFriend
+    }
+}
+
+struct Student {
+    var name: String
+    var bestFriend: String
+    
+    init(name studentName: String, bestFriend studentBestFriend: String) {
+        print("Enrolling \(studentName) in class...")
+        name = studentName
+        bestFriend = studentBestFriend
+    }
+}
