f6432a052689ea0dac60ca6a269881056bc32bd0
Aleksandr Morozov <dr.morozov@hotmail.com>
2024-09-09 22:56:05 -0700

    Day 19 finished

---
+++ Day 19/TempConverter/TempConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TempConverter
+//
+//  Created by Aleksandr Morozov on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 19/TempConverter/TempConverter/TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterApp.swift
+//  TempConverter
+//
+//  Created by Aleksandr Morozov on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
