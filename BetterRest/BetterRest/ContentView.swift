b3ff72b660cd85613a274edfbede5a891747d72b
Aleksandr Morozov <dr.morozov@hotmail.com>
2024-10-02 23:11:59 -0700

    Day 26 Finished

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(Date.now.formatted(date: .long, time: .shortened))
+    }
+    
+    func exampleDates() {
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
