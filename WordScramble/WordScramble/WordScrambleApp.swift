191150355583eb55abe978b0ec0ae602caf891af
Aleksandr Morozov <dr.morozov@hotmail.com>
2024-10-09 23:01:34 -0700

    Day 28 Finished

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Aleksandr Morozov on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
