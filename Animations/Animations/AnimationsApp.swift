c519960a7314c9c5ff27a7738214812a1fc8b823
Aleksandr Morozov <dr.morozov@hotmail.com>
2024-10-29 23:05:40 -0700

    Day 32

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Aleksandr Morozov on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/AnimationsTests/AnimationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsTests.swift
+//  AnimationsTests
+//
+//  Created by Aleksandr Morozov on 10/29/24.
+//
+
+import Testing
+@testable import Animations
+
+struct AnimationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
