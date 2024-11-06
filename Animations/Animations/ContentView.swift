97c7b5bb3beef55dd0633cb76c74a2c7de36fdf3
Aleksandr Morozov <dr.morozov@hotmail.com>
2024-11-05 23:36:29 -0800

    Day 33 in Progress

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,27 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        print(animationAmount)
-        
-        return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(
-                .easeInOut(duration: 1)
-                .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            
-            Spacer()
-            
-            Button("Tap Me") {
-                animationAmount += 1
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+            Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
             }
-            .padding(40)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
         }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+            )
     }
 }
 
